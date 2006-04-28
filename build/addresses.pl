c2f1d48e754450a2f5161562fb553fbca2749781
Ian Young <ian@iay.org.uk>
2006-04-28 13:05:04 +0000

    Rewrite so that we can detect necessary additions and removals.

--- build/addresses.pl
+++ build/addresses.pl
@@ -1,18 +1,82 @@
 #!/usr/bin/perl
 
+#
+# Load list addresses.
+#
+open(LIST, "list.txt") || die "could not open list addresses file";
+while (<LIST>) {
+	chomp; # remove \n
+	$list{$_} = 1 unless $_ eq '';
+}
+close LIST;
+
+#
+# Load extra addresses.
+#
+open(EXTRAS, "extra_addresses.txt") || die "could not open extra addresses file";
+while (<EXTRAS>) {
+	chomp;	# remove \n
+	$extras{$_} = 1 unless $_ eq '';
+}
+close EXTRAS;
+
+#
+# Load addresses from the metadata.
+#
 open(XML,"../xml/sdss-metadata-unsigned.xml") || die "could not open input file";
-
 while (<XML>) {
-    if (/<EmailAddress>mailto:(.*)<\/EmailAddress>/) {
-	if (!defined($lowered{lc $1})) {
-	    $lowered{lc $1} = $1;
-	    push @addresses, $1;
+	if (/<EmailAddress>(mailto:)?(.*)<\/EmailAddress>/) {
+		$metadata{$2} = 1;
 	}
-    }
 }
-
-foreach $addr (@addresses) {
-    print $addr, "\n";
-}
-
 close XML;
+
+#
+# Now figure out the addresses we want to see in the mailing list.
+# Make them lower case for comparisons.
+#
+foreach $addr (keys %extras) {
+	$wanted{lc $addr} = $addr;
+}
+foreach $addr (keys %metadata) {
+	$wanted{lc $addr} = $addr;
+}
+
+#
+# Similar lower-case hash for the current list.
+#
+foreach $addr (keys %list) {
+	$have{lc $addr} = $addr;
+}
+
+#
+# Cancel the ones that are *in* the mailing list from the wanted
+# collection.  Whine about (now) unwanted entries in the
+# mailing list.
+#
+$first = 1;
+foreach $addr (keys %have) {
+	my $a = $have{$addr};
+	if (defined($wanted{$addr})) {
+		delete $wanted{$addr};
+	} else {
+		if ($first) {
+			$first = 0;
+			print "\nDelete unwanted: \n";
+		}
+		print "$a\n";
+	}
+}
+
+#
+# List the ones that are wanted, but not yet in the list.
+#
+$first = 1;
+foreach $addr (keys %wanted) {
+	my $a = $wanted{$addr};
+	if ($first) {
+		$first = 0;
+		print "\nAdd wanted: \n";
+	}
+	print "$a\n";
+}
