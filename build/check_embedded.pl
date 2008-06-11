66ed027d1a96ea8f2d5730b30b0dd1b596b6df6c
Ian Young <ian@iay.org.uk>
2008-06-11 14:33:03 +0000

    Look at expiry date on embedded certificates: flag ones which have expired, warn about ones close to expiry (30 days) and show ones within 60 days of expiry.

--- build/check_embedded.pl
+++ build/check_embedded.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 use File::Temp qw(tempfile);
+use Date::Parse;
 
 while (<>) {
 
@@ -76,6 +77,19 @@ while (<>) {
 				# print "   Public key size: $pubSize\n";
 				if ($pubSize < 1024) {
 					print "      *** PUBLIC KEY TOO SHORT ***\n";
+				}
+			}
+			if (/Not After : (.*)$/) {
+				$notAfter = $1;
+				$days = (str2time($notAfter)-time())/86400.0;
+				if ($days < 0) {
+					print "   *** EXPIRED ***\n";
+				} elsif ($days < 30) {
+					$days = int($days);
+					print "   *** expires in $days days\n";
+				} elsif ($days < 90) {
+					$days = int($days);
+					print "   expires in $days days\n";
 				}
 			}
 		}
