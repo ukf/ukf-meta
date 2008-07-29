575be62e8696afc99854358752928ca94ba12269
Ian Young <ian@iay.org.uk>
2008-07-29 16:35:02 +0000

    Add a tool to remove the old Eduserv gateway certificate from all affected entities once it has expired.

---
+++ build/remove_old_eduserv_cert.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+
+#
+# The input file is a fragment file that may or may not need to have
+# the old Eduserv gateway certificate removed from it.  Remove the certificate if
+# required, or just re-export the file unchanged.
+#
+
+# This line indicates that the old certificate is present
+$old_cert_line = 'MIIDaTCCAtKgAwIBAgIQLqPCly3VfA8B2xVsTv59ajANBgkqhkiG9w0BAQUFADCB';
+
+while (<>) {
+
+	if (/$old_cert_line/) {
+		$delete = 1;
+	}
+	
+	if ($ended) {
+		print $_;
+	} else {
+		push @lines, $_;
+		if ($delete && /<\/KeyDescriptor/) {
+			while ((pop @lines) !~ /<KeyDescriptor/) {
+				# remove the KeyDescriptor body back to its initial line
+			}
+			undef $delete;
+		}
+	}
+	
+	# at the end...
+	if (/<\/EntityDescriptor>/) {
+		# re-export the old file
+		while ($line = shift @lines) {
+			print $line;
+		}
+		$ended = 1;
+	}
+
+}
+
+# end
