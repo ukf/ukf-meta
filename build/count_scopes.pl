e377b9ef2b8ddc3a7828c4196550b8bb6d1a5c6b
Ian Young <ian@iay.org.uk>
2009-08-14 18:39:55 +0000

    Update script to latest Xalan.

--- build/count_scopes.pl
+++ build/count_scopes.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-open(XML,"java -cp ../xalan-j_2_6_0/bin/xalan.jar org.apache.xalan.xslt.Process -IN ../xml/ukfederation-metadata-unsigned.xml -XSL extract_scopes.xsl|") || die "could not open input file";
+open(XML,"java -Djava.endorsed.dirs=../tools/xalan/endorsed org.apache.xalan.xslt.Process -IN ../xml/ukfederation-metadata-unsigned.xml -XSL extract_scopes.xsl|") || die "could not open input file";
 while (<XML>) {
 	# print $_;
 	chop;
