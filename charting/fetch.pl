7a1bc01aa3c25d063091ef8ade8d9fcf15dae8a7
Ian Young <ian@iay.org.uk>
2009-12-14 16:41:58 +0000

    Extend for December 2009.  Also cache statistics HTML files.

--- charting/fetch.pl
+++ charting/fetch.pl
@@ -6,6 +6,7 @@
 use File::stat;
 
 $fn = '../xml/ukfederation-metadata.xml';
+$fn2 = '../xml/ukfederation-stats.html';
 
 @months = (
 	'2006-12',
@@ -14,12 +15,14 @@ $fn = '../xml/ukfederation-metadata.xml';
 	'2008-01', '2008-02', '2008-03', '2008-04', '2008-05', '2008-06',
 	'2008-07', '2008-08', '2008-09', '2008-10', '2008-11', '2008-12',
 	'2009-01', '2009-02', '2009-03', '2009-04', '2009-05', '2009-06',
-	'2009-07', '2009-08', '2009-09', '2009-10', '2009-11',	
+	'2009-07', '2009-08', '2009-09', '2009-10', '2009-11', '2009-12',
 );
 
 foreach $month (@months) {
 	print "Fetching $month...";
 	system("svn update $fn --quiet --revision \\{$month-01T00:00:00Z\\}");
 	system("cp $fn cache/$month.xml");
+	system("svn update $fn2 --quiet --revision \\{$month-01T00:00:00Z\\}");
+	system("cp $fn2 cache/$month.html");
 	print "\n";
 }
