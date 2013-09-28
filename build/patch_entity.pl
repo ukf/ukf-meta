090985e609936ba504ab26d6f35308e767b10d5d
Ian Young <ian@iay.org.uk>
2013-09-28 13:01:54 +0000

    Remove AthensPUIDAuthority from all fragment files.

--- build/patch_entity.pl
+++ build/patch_entity.pl
@@ -1,13 +1,8 @@
 #!/usr/bin/perl
 
-$seen = 0;
-
 while (<>) {
-	if (/^\s+http:\/\/ukfederation.org.uk\/2006\/11\/label/) {
-		$seen = 1; # don't apply the change twice to the same file
-	}
-	if (!$seen && /^(\s+)http:\/\/www.w3.org\/2001\/04\/xmlenc/) {
-		print "$1http://ukfederation.org.uk/2006/11/label uk-fed-label.xsd\n";
+	if (/AthensPUIDAuthority/) {
+		next;
 	}
 	print $_;
 }
