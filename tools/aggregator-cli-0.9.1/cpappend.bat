4f808bc4e591b29e44948ca5a39a9c2ee740df8c
Ian Young <ian@iay.org.uk>
2016-04-25 16:08:30 +0000

    Upgrade to Shib MDA 0.9.1 for public version of CRDetectionStage.

---
+++ tools/aggregator-cli-0.9.1/cpappend.bat
@@ -0,0 +1,17 @@
+rem ---------------------------------------------------------------------------
+rem Append to CLASSPATH
+rem ---------------------------------------------------------------------------
+
+rem Process the first argument
+if ""%1"" == """" goto end
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%1
+shift
+
+rem Process the remaining arguments
+:setArgs
+if ""%1"" == """" goto doneSetArgs
+set LOCALCLASSPATH=%LOCALCLASSPATH% %1
+shift
+goto setArgs
+:doneSetArgs
+:end
