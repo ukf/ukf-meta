66773b0c1e69906c85c71634cdf045dba148c452
Rhys Smith <rhys.smith@jisc.ac.uk>
2016-10-29 13:36:18 +0100

    Tidy indentation of stats script function

--- utilities/stats-generate.sh
+++ utilities/stats-generate.sh
@@ -18,16 +18,16 @@
 
 bytestohr()
 {
-        value=$1
-        valueint=$1
-        i=0
-        suffix=" KMGTPEZY"
-        while [ $valueint -gt 1024 ]; do
-                i=$((i+1))
-                valueint=$((valueint/1024))
-                value=$(echo "scale=1;$value/1024" | bc)
-        done
-        echo $value ${suffix:$i:1}B
+    value=$1
+    valueint=$1
+    i=0
+    suffix=" KMGTPEZY"
+    while [ $valueint -gt 1024 ]; do
+        i=$((i+1))
+        valueint=$((valueint/1024))
+        value=$(echo "scale=1;$value/1024" | bc)
+    done
+    echo $value ${suffix:$i:1}B
 }
 
 
