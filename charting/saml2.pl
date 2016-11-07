7b6c3f58b0f83130b30db58218cf9eed68755373
Ian Young <ian@iay.org.uk>
2016-11-07 10:55:30 +0000

    Fix multiple month display of SAML 2 entities.
    Note that this won't go all the way back to the start of records
    because the XSLT depends on published metadata containing
    registrationAuthority metadata, which we started using
    in September 2012.

--- charting/saml2.pl
+++ charting/saml2.pl
@@ -27,11 +27,21 @@ foreach $month (@months) {
 	open(TXT, xalanCall . " -IN $fn -XSL saml2.xsl|") || die "could not open input file";
 	$_ = <TXT>;
 	chop;
+	# print "$month: $_\n";
 	my ($entities, $idps, $sps, $saml2total, $saml2idp, $saml2sp) = split;
-	push @overallRatio, $saml2total/$entities;
-	push @idpRatio, $saml2idp/$idps;
-	push @spRatio, $saml2sp/$sps;
-	push @product, ($saml2idp/$idps)*($saml2sp/$sps);
+	if ($entities == 0) {
+		# print "skipping $month: $_\n";
+		next;
+	}
+	my $mPrefix = $allMonths ? "$month: " : '';
+	my $oRatio = $saml2total/$entities;
+	push @overallRatio, "$mPrefix$oRatio";
+	my $iRatio = $saml2idp/$idps;
+	push @idpRatio, "$mPrefix$iRatio";
+	my $sRatio = $saml2sp/$sps;
+	push @spRatio, "$mPrefix$sRatio";
+	my $p = ($saml2idp/$idps)*($saml2sp/$sps);
+	push @product, "$mPrefix$p";
 	close TXT;
 }
 
