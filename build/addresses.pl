03ae329aec83b4dbe1ac771bcca4b2f7a18ddeb8
Ian Young <ian@iay.org.uk>
2017-01-18 16:21:30 +0000

    Generate mailing list appropriate to Jiscmail
    Resolves #108.

--- build/addresses.pl
+++ build/addresses.pl
@@ -3,16 +3,6 @@
 use Xalan;
 
 #
-# Load list addresses.
-#
-open(LIST, "list.txt") || die "could not open list addresses file";
-while (<LIST>) {
-	chomp; # remove \n
-	$list{$_} = 1 unless $_ eq '';
-}
-close LIST;
-
-#
 # Load extra addresses.
 #
 # One extra address per line.  Blank lines and lines starting with '#' are
@@ -53,40 +43,11 @@ foreach $addr (keys %metadata) {
 }
 
 #
-# Similar lower-case hash for the current list.
+# List all wanted addresses.
 #
-foreach $addr (keys %list) {
-	$have{lc $addr} = $addr;
-}
-
-#
-# Cancel the ones that are *in* the mailing list from the wanted
-# collection.  Whine about (now) unwanted entries in the
-# mailing list.
-#
-$first = 1;
-foreach $addr (sort keys %have) {
-	my $a = $have{$addr};
-	if (defined($wanted{$addr})) {
-		delete $wanted{$addr};
-	} else {
-		if ($first) {
-			$first = 0;
-			print "\nDelete unwanted: \n";
-		}
-		print "$a\n";
-	}
-}
-
-#
-# List the ones that are wanted, but not yet in the list.
-#
-$first = 1;
-foreach $addr (keys %wanted) {
+print "--- LIST BEGIN ---\n";
+foreach $addr (sort keys %wanted) {
 	my $a = $wanted{$addr};
-	if ($first) {
-		$first = 0;
-		print "\nAdd wanted: \n";
-	}
 	print "$a\n";
 }
+print "--- LIST END ---\n";
