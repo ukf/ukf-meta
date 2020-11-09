fdebe00a1a944fc83fc0d776dd0209c726eee130
Alex Stuart <alex.stuart@jisc.ac.uk>
2020-11-09 20:53:40 +0000

    Change É into HTML character entity reference in generate.html.orgnamescope
    See ukf/ukf-meta#258 for details.

---
+++ utilities/bodge-eacute.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w -p -i
+#
+# É is changed to HTML character entity reference &Eacute;
+#
+# An awful bodge to fix output of IdPInfoList. Our instance of
+# PMwiki encodes pages in windows-1252 (single byte character encoding),
+# and IdPInfoList includes a file encoded in UTF-8. All is well until
+# we have a 2-byte character in GÉANT.
+# 
+binmode(STDOUT, ":encoding(UTF-8)");
+s/É/&Eacute;/g;
