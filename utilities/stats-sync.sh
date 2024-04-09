954699a087ee362b3f33af52f12f06afd8c344c6
Alex Stuart <alex.stuart@jisc.ac.uk>
2024-04-09 12:12:09 +0100

    Removed rsync of logfiles from Test SP.
    See ukf/ukf-meta#423 for details

--- utilities/stats-sync.sh
+++ utilities/stats-sync.sh
@@ -23,13 +23,9 @@ rsync -at --exclude modsec* stats@www-we-01:/var/log/httpd/* $logslocation/www/w
 rsync -at --exclude modsec* stats@test-idp:/var/log/httpd/* $logslocation/test-idp/
 rsync -at stats@test-idp:/opt/shibboleth-idp/logs/idp-audit* $logslocation/test-idp/
 
-# Logs from Test SP
+# Logs from Test SP inaccessible once the Test SP migrated to 2024 infrastructure
 #
 # The Test SP has a cronjob to remove logs with PII > 30 days old, we replicate that in this script
-#
-rsync -at --exclude modsec* stats@test-sp:/var/log/httpd/* $logslocation/test-sp/
-rsync -at stats@test-sp:/var/log/shibboleth/shibd* $logslocation/test-sp/
-rsync -at stats@test-sp:/var/log/shibboleth/transaction* $logslocation/test-sp/
 find $logslocation/test-sp/ -type f -mtime +90 -delete
 
 # Exit happily
